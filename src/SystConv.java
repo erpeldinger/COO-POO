dd6e8a03473a9690dbd27fe2eb1ff2b81dee7766
cla <clarisse.erpeldinger@gmail.com>
2019-11-28 10:28:57 +0100

    Modification des classes bleues2

--- src/SystConv.java
+++ src/SystConv.java
@@ -9,13 +9,13 @@ public class SystConv{
     }
 
     //Methodes
-    private void askPseudo(Struct U2 user){}
+    private void askPseudo(U2 user){}
 
-    private void askPwd(Struct U2 user){}
+    private void askPwd(U2 user){}
 
     private void createUser(int id, String pwd){}
 
-    private void sendM(String msg, Struct U2 user){}
+    private void sendM(String msg, U2 user){}
 
     private void updateConnectedUL(int id){}
 
@@ -24,16 +24,16 @@ public class SystConv{
         return exist;
     }
 
-    private <Struct Conv> getHistorique(Struct U2 user1, Struct U2 user2){
+    private <Struct Conv> getHistorique(U2 user1, U2 user2){
         hist = new StructConv();
         return hist;
     }
 
-    private void updateHistConv(Struct Conv listNewM){}
+    private void updateHistConv(Conv listNewM){}
 
-    private void notifyModifiedPseudo(Struct U2 user){}
+    private void notifyModifiedPseudo(U2 user){}
 
-    private int findId(Struct U2 user){
+    private int findId(U2 user){
         id = 0;
         return id;
     }
