f15c34b59739e3bcc11734f5c1371d8afd410e43
erpeldinger <clarisse.erpeldinger@gmail.com>
2020-01-11 10:30:53 +0100

    test DemoTCPServer et Client

--- src/demo/DemoClientTCP.java
+++ src/demo/DemoClientTCP.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.*;
 import user.User;
 import communication.*;
@@ -9,13 +11,37 @@ public class DemoClientTCP {
     public static void main (String[] args) throws SocketException, Exception {
     	
     	//CLIENT 
+    	/*
     	int port = 1234;
     	InetAddress addr = InetAddress.getLocalHost();
+        System.out.println("addr ok");
         Socket socket = new Socket(addr,port); 
+        System.out.println("socket cree");
     	User u1 = new User(88,"UserEnvoieBroadcast","mdp",1246,addr);
     	TCPClient server = new TCPClient(socket,u1);
     	
     	server.sendMessage("Coucou c'est client, ca boom ?");
+    	*/
+    	
+    	Socket sockC = null;
+        OutputStream outC = null;
+        InputStream inC = null;
+        InetAddress addrC = null;
+        try {
+            addrC = InetAddress.getLocalHost();
+            sockC = new Socket(addrC, 1234); 
+            System.out.println("Client 1 ok : 1234");
+        }
+        catch (Exception e){
+            System.out.println("Erreur création du socket client user1");
+        }
+        try {
+            outC = sockC.getOutputStream();
+            inC = sockC.getInputStream();
+        }
+        catch (Exception e){
+            System.out.println("Erreur création des inC et outC");
+        }
     	
     }
 }
--- src/demo/DemoServerTCP.java
+++ src/demo/DemoServerTCP.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.*;
 import user.User;
 import communication.*;
@@ -9,10 +11,34 @@ public class DemoServerTCP {
     public static void main (String[] args) throws SocketException, Exception {
     	
     	//Server
+    	/*
     	int port = 1234;
     	InetAddress addr = InetAddress.getLocalHost();
     	User u2 = new User(99,"UserEnvoieBroadcast","mdp",1288,addr);
     	TCPServer server = new TCPServer(port);
+    	*/
     	
+    	ServerSocket serverSocket = null;
+        Socket sockS = null;
+        OutputStream outS = null;
+        InputStream inS = null;
+        System.out.println("init ok");
+        try {
+            serverSocket = new ServerSocket(1234); 
+            System.out.println("server socket ok");
+            sockS = serverSocket.accept();
+            System.out.println("Serveur 1 ok : 1234");
+        }
+        catch (Exception e){
+            System.out.println("Erreur création du server1");
+        }
+        try {
+            outS = sockS.getOutputStream();
+            inS = sockS.getInputStream();
+            System.out.println("stream ok");
+        }
+        catch (Exception e){
+            System.out.println("Erreur création des inS1 et outS1");
+        }
     }
 }
