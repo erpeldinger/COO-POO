3fb40fbe73bac0f47b62d443c9ead13bd2c654c3
Chatelier Cerise <chatelie@insa-11292>
2020-01-25 18:38:08 +0100

    ports dans database

--- src/communication/ListenerUDP.java
+++ src/communication/ListenerUDP.java
@@ -111,10 +111,10 @@ public class ListenerUDP extends Thread {
 		    		socket.send(outPacket);
 		    		//System.out.println("[LISTENER UDP] If -> end");
 		    		
-		    		//System.out.println("[LISTENER UDP] avant luc ");
+		    		System.out.println("[LISTENER UDP] avant luc ");
 		    		//ajout de l'user dans la LUC
 		    		this.userLUC.add(Message.toMessageBdc(msg).getId());	
-		    		//System.out.println("[LISTENER UDP] apres luc");
+		    		System.out.println("[LISTENER UDP] apres luc");
 		    		//Ajout de l'adresse IP de user dans la bdd 
 		    		Connect.createNewDatabase("database.db");
 		        	Connect.createNewTableLUC("database.db");
