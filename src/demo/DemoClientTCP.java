a95b060f3cb21e57aa8673dfaa61f47e79aea9e2
Chatelier Cerise <chatelie@insa-11294>
2020-01-25 21:34:17 +0100

    demo tcp

--- src/demo/DemoClientTCP.java
+++ src/demo/DemoClientTCP.java
@@ -4,22 +4,24 @@ import java.io.IOException;
 import java.net.*;
 import user.User;
 import communication.*;
-
+import requete.*;
 
 public class DemoClientTCP {
     
     public static void main (String[] args) throws SocketException, Exception, IOException, SecurityException, IllegalArgumentException, NullPointerException {
     	
     	
-
+    	Connect.createNewDatabase("database.db");
+    	Connect.createNewTableUser("database.db");
+    	Connect.insertUser("database.db", "ClientTom", "mdp", 13);
     	//Broadcast
     	User u2 = new User(13,"ClientTom","mdp",1234);
-    	Thread.sleep(500);
-        u2.allowBroadcast(new BroadcastingClient(u2.getListener().getDatagramSocket(),1234, u2)); //port du Server ListenerUDP (1234 pour tous)
+    	//Thread.sleep(500);
+        //u2.allowBroadcast(new BroadcastingClient(u2.getListener().getDatagramSocket(),1234, u2)); //port du Server ListenerUDP (1234 pour tous)
         
         
         //Partie TCP
-        InetAddress ip = InetAddress.getByName("10.1.5.229"); // en dur [celle du serveur]
+        InetAddress ip = InetAddress.getByName("10.1.5.183"); // en dur [celle du serveur]
         System.out.println("IP Client : " +ip);
     	int port = 2010;
     	TCPClient client = new TCPClient(ip,2000,u2, 12);
